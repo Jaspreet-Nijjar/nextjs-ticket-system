10e778ecc2c9e1f58477269bcb4fbdbe4a6865b8
Jaspreet <jaspreetkn92@gmail.com>
2024-09-11 09:11:35 +0100

    added Rubik font

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,17 +1,10 @@
 import localFont from 'next/font/local';
 import './globals.css';
-import Link from 'next/link';
 import Navbar from './components/Navbar';
+import { Rubik } from 'next/font/google';
 
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
+const rubik = Rubik({
+  subsets: ['latin'],
 });
 
 export const metadata = {
@@ -22,9 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={rubik.className}>
         <Navbar />
         {children}
       </body>
