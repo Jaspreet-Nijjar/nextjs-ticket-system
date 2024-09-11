ac9d7a483b4be0390af5bd62fc3d9d042a978a26
Jaspreet <jaspreetkn92@gmail.com>
2024-09-11 09:20:46 +0100

    added image to navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,8 +1,17 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 const Navbar = () => {
   return (
     <nav>
+      <Image
+        src="/dojo-logo.png"
+        alt="dojo logo"
+        width={70}
+        height={70}
+        quality={100}
+        placeholder="blur"
+      />
       <h1>Dojo Helpdesk</h1>
       <Link href="/">Dashboard</Link>
       <Link href="/tickets">Tickets</Link>
